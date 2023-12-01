8bfb2a507be053e4fba26c8cfee9d0265ba1a96b
Jonathan Holm <jonathan.holm@omikai.com>
2023-12-01 10:12:31 +0100

    Conf prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    trailingComma: 'none',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    arrowParens: 'always',
+    printWidth: 150
+};
